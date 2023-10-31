e05134ec40c1f44032e6e15489f955f2125419ca
Paula Gaspar <gasparpaula147@gmail.com>
2023-10-31 09:43:28 -0300

    style: menu finalizado

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 const botao = document.getElementById('nav-icon3');
+const navbar = document.getElementById('navbar');
+const menu = document.getElementById('menu');
+const overlay = document.getElementById('overlay');
+const body = document.querySelector('body');
+const main = document.querySelector('main');
 
 botao.addEventListener('click', () => {
     botao.classList.toggle('open');
+    setTimeout(() => {
+        navbar.classList.toggle('navbar-fixada');
+        menu.classList.toggle('menu-aberto');
+        overlay.classList.toggle('overlay-ativado');
+        body.classList.toggle('scroll-bloqueado');
+    }, 150)
 })
